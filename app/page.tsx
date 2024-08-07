3208c49e0b05f7e7eec5bc9445615198c8c0a931
Mauro Lucas <maurolucas2014@gmail.com>
2024-08-07 20:37:52 -0300

    feat: add BokingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,9 @@ import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar, AvatarImage } from "./_components/ui/avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/BarbershopItem";
+import BokingItem from "./_components/BokingItem";
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
@@ -26,34 +25,28 @@ export default async function Home() {
             </Button>
         </div>
 
+        <div className="flex gap-3 mt-6">
+          <Button className="gap-2" variant="secondary">
+            <Image src="/cabelo.svg" alt="Imagem Cabelo" width={16} height={16}/>
+            Cabelo
+          </Button>
+          
+          <Button className="gap-2" variant="secondary">
+            <Image src="/barba.svg" alt="Imagem Barba" width={16} height={16}/>
+            Barba
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="/acabamento.svg" alt="Imagem Acabamento" width={16} height={16}/>
+            Cabelo
+          </Button>
+        </div>
+
         <div className="relative w-full h-[150px] mt-6">
           <Image src="/banner-01.png" alt="Imagem Banner" fill className="object-cover rounded-xl" />
         </div>
 
-        <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">Agendamentos</h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3>Corte de Cabelo</h3>
-
-              <div className="flex items-center gap-2">
-
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"/>
-                </Avatar>
-                <p className="text-sm">Barbearia Full Stack</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BokingItem />
 
         <h2 className="text-xs font-bold uppercase text-gray-400 mt-6 mb-3">Recomendados</h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
